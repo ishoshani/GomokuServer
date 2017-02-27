64ec447ca73b7e4f5767b7c62b7bc247cc9d41e3
isho <idoshoshani@gmail.com>
2017-02-27 14:23:04 -0800

    added server messages

--- ClientProtocol.java
+++
@@ -1,9 +0,0 @@
-package com.example.isho.gomoku8;
-
-class ClientProtocol{
-  public static void processProcedure(GamePacket input){
-    if(input.packetType.equals("OPENCONNECTION")){
-      System.out.println("Connection successful");
-    }
-  }
-}
